ddfe9980baf53afd5e1addc0690b2b4b0af6c754
lakshitp1104 <lakshitpatil13@gmail.com>
2026-01-22 23:12:18 +0530

    commit

---
+++ 01-frontend/01-html/day18/script.js
@@ -0,0 +1,16 @@
+//Array code
+let skills = [ "html", "css", "js", "react"];
+console.log("Skills:", skills);
+console.log("First Skill:", skills[0]);
+
+
+//Object code
+let student = {
+  name: "Lakshit",
+  semester: 6,
+  branch: "CSE",
+  skills: ["HTML", "CSS", "JavaScript"]
+};
+
+console.log(student.name);
+console.log(student.skills[2]);
