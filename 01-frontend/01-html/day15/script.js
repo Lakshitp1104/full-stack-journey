5db18640e05b60e0ed82fe5f6c834772357d1649
lakshitp1104 <lakshitpatil13@gmail.com>
2026-01-22 22:38:11 +0530

    commit

---
+++ 01-frontend/01-html/day15/script.js
@@ -0,0 +1,15 @@
+// Declare variables
+let name = "Lakshit";
+let course = "Computer Science Engineering";
+let goal = "Become a Full Stack Developer";
+
+// Log values to console
+console.log(name);
+console.log(course);
+console.log(goal);
+
+// Change one value
+goal = "Crack a good placement as a Software Developer";
+
+// Log updated value
+console.log(goal);
