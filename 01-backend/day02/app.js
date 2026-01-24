2137c28c611c7f161a8d18dd3facb3fa07c2e302
lakshitp1104 <lakshitpatil13@gmail.com>
2026-01-24 22:26:57 +0530

    commit

---
+++ 01-backend/day02/app.js
@@ -0,0 +1,6 @@
+// Day 2 - Using require()
+
+const math = require("./math");
+
+console.log("Addition:", math.add(10, 5));
+console.log("Subtraction:", math.subtract(10, 5));
---
+++ 01-backend/day02/math.js
@@ -0,0 +1,15 @@
+// Day 2 - Custom Module
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+// Export functions
+module.exports = {
+  add,
+  subtract
+};
