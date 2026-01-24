794d4677046f5f34306e96f04b21c915b5d949ce
lakshitp1104 <lakshitpatil13@gmail.com>
2026-01-24 22:16:28 +0530

    commit

---
+++ 01-backend/day01/hello.js
@@ -0,0 +1,18 @@
+// Day 1 Backend - Node.js Basics
+
+console.log("Hello Backend World");
+
+// Variables
+let name = "Lakshit";
+let role = "Backend Developer (Beginner)";
+let day = 1;
+
+// Output
+console.log("Name:", name);
+console.log("Role:", role);
+console.log("Day:", day);
+
+// Simple calculation
+let a = 10;
+let b = 5;
+console.log("Sum:", a + b);
